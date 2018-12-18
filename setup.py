9803668fb87207c95c5c643b327f9fdae1565fef
Jim Crist <jiminy.crist@gmail.com>
2018-12-18 16:16:15 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,7 @@ from setuptools import setup
 with open('README.rst') as f:
     long_description = f.read()
 
-requirements = [
-    'jupyterhub',
-    'skein',
-]
-
-setup(name='yarnspawner',
+setup(name='jupyterhub-yarnspawner',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       license='BSD',
@@ -17,9 +12,18 @@ setup(name='yarnspawner',
       maintainer_email='jiminy.crist@gmail.com',
       description='JupyterHub Spawner for Apache Hadoop/YARN Clusters',
       long_description=long_description,
+      url='http://github.com/jcrist/yarnspawner',
+      project_urls={
+          'Source': 'https://github.com/jcrist/yarnspawner',
+          'Tracker': 'https://github.com/jcrist/yarnspawner/issues',
+      },
       packages=['yarnspawner'],
-      install_requires=requirements,
+      python_requires='>=3.5',
+      install_requires=[
+          'jupyterhub>=0.8',
+          'skein',
+      ],
       entry_points='''
-        [console_scripts]
-        yarnspawner-singleuser=yarnspawner.singleuser:main
+          [console_scripts]
+          yarnspawner-singleuser=yarnspawner.singleuser:main
       ''')
