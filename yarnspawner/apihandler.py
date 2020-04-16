f95bfdf18b2e6b099e8a5b2d31c9c8f60d179a9f
Sergey Zhemzhitsky <szhemzhitski@gmail.com>
2020-04-16 13:11:18 +0300

    support of allow_named_servers setting

--- yarnspawner/apihandler.py
+++ yarnspawner/apihandler.py
@@ -1,4 +1,3 @@
-import json
 from tornado import web
 from jupyterhub.apihandlers import APIHandler, default_handlers
 
@@ -12,9 +11,24 @@ class YarnSpawnerAPIHandler(APIHandler):
         user = self.current_user
         data = self.get_json_body()
         port = int(data.get('port', 0))
-        user.spawner.current_port = port
-        self.finish(json.dumps({"message": "YarnSpawner port configured"}))
-        self.set_status(201)
+        name = data.get('name', '')
+
+        self.log.debug("Registering port number %i for spawner '%s' of user '%s'", port, name, user.name)
+
+        resp_code = 201
+        resp_body = {"message": "YarnSpawner port configured"}
+        if name:
+            # if allow_named_servers
+            if name in user.spawners:
+                user.spawners[name].current_port = port
+            else:
+                resp_code = 400
+                resp_body = {"message": "YarnSpawner not found for named server {}".format(name)}
+        else:
+            user.spawner.current_port = port
+
+        self.finish(resp_body)
+        self.set_status(resp_code)
 
 
 default_handlers.append((r"/api/yarnspawner", YarnSpawnerAPIHandler))
