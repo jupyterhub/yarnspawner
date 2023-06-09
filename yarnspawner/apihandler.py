aaaaffd8db5e563968e490298d34fe48dd628301
Joao Amaral <7281460+joaopamaral@users.noreply.github.com>
2023-06-09 17:37:02 -0300

    Reverting some changes

--- yarnspawner/apihandler.py
+++ yarnspawner/apihandler.py
@@ -4,28 +4,16 @@ from jupyterhub.apihandlers import APIHandler, default_handlers
 
 
 # Borrowed and modified from jupyterhub/batchspawner:
-# https://github.com/jupyterhub/batchspawner/blob/d5f9a0bbaa92748267bb1dd2cc4d4a0436670482/batchspawner/api.py
+# https://github.com/jupyterhub/batchspawner/blob/d1052385f2/batchspawner/api.py
 class YarnSpawnerAPIHandler(APIHandler):
     @web.authenticated
     def post(self):
-        """POST set user spawner data"""
-        if hasattr(self, "current_user"):
-            # Jupyterhub compatability, (september 2018, d79a99323ef1d)
-            user = self.current_user
-        else:
-            # Previous jupyterhub, 0.9.4 and before.
-            user = self.get_current_user()
-        token = self.get_auth_token()
-        spawner = None
-        for s in user.spawners.values():
-            if s.api_token == token:
-                spawner = s
-                break
+        """POST set user's spawner port number"""
+        user = self.current_user
         data = self.get_json_body()
-        for key, value in data.items():
-            if hasattr(spawner, key):
-                setattr(spawner, key, value)
-        self.finish(json.dumps({"message": "YarnSpawner data configured"}))
+        port = int(data.get('port', 0))
+        user.spawner.current_port = port
+        self.finish(json.dumps({"message": "YarnSpawner port configured"}))
         self.set_status(201)
 
 
--- yarnspawner/jupyter_labhub.py
+++ yarnspawner/jupyter_labhub.py
@@ -1,15 +1,26 @@
 import os
-import sys
-from runpy import run_path
-from shutil import which
 
-import requests
 from jupyterhub.utils import random_port, url_path_join
-from jupyterhub.services.auth import HubAuth
+from traitlets import default
+
+try:
+    from jupyterlab.labhubapp import SingleUserLabApp
+except ImportError:
+    raise ImportError("You must have jupyterlab installed for this to work")
 
 
-# Borrowed and modified from jupyterhub/batchspawner:
-# https://github.com/jupyterhub/batchspawner/blob/d5f9a0bbaa92748267bb1dd2cc4d4a0436670482/batchspawner/singleuser.py
+class YarnSingleUserLabApp(SingleUserLabApp):
+    @default('port')
+    def _port(self):
+        return random_port()
+
+    def start(self):
+        self.hub_auth._api_request(method='POST',
+                                   url=url_path_join(self.hub_api_url, 'yarnspawner'),
+                                   json={'port': self.port})
+        super().start()
+
+
 def main(argv=None):
     # Set configuration directory to something local if not already set
     for var in ['JUPYTER_RUNTIME_DIR', 'JUPYTER_DATA_DIR']:
@@ -17,32 +28,7 @@ def main(argv=None):
             if not os.path.exists('.jupyter'):
                 os.mkdir('.jupyter')
             os.environ[var] = './.jupyter'
-
-    port = random_port()
-    hub_auth = HubAuth()
-    hub_auth.client_ca = os.environ.get("JUPYTERHUB_SSL_CLIENT_CA", "")
-    hub_auth.certfile = os.environ.get("JUPYTERHUB_SSL_CERTFILE", "")
-    hub_auth.keyfile = os.environ.get("JUPYTERHUB_SSL_KEYFILE", "")
-
-    url = url_path_join(hub_auth.api_url, "yarnspawner")
-
-    # internal_ssl kwargs
-    kwargs = {}
-    if hub_auth.certfile and hub_auth.keyfile:
-        kwargs["cert"] = (hub_auth.certfile, hub_auth.keyfile)
-    if hub_auth.client_ca:
-        kwargs["verify"] = hub_auth.client_ca
-
-    requests.post(
-        url,
-        headers={"Authorization": f"token {hub_auth.api_token}"},
-        json={"port": port},
-        **kwargs,
-    )
-
-    cmd_path = which(sys.argv[1])
-    sys.argv = sys.argv[1:] + ["--port={}".format(port)]
-    run_path(cmd_path, run_name="__main__")
+    return YarnSingleUserLabApp.launch_instance(argv)
 
 
 if __name__ == "__main__":
